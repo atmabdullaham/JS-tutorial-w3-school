3a7badad9027086a801f1703a910f23d8e436442
atmabdullaham <atmabdullaham@gmail.com>
2024-11-26 23:57:27 +0600

    JavaScript Function Syntax added

--- functions.js
+++ functions.js
@@ -10,3 +10,29 @@ function myFunction(p1, p2) {
 }
 
 console.log(myFunction(10, 2));
+
+/**
+ * JavaScript Function Syntax: 
+ * 
+ * A JavaScript functions is defined with the function keyword, followed by a name, followed by parentheses();
+ * 
+ * --Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+ * 
+ * --The parentheses may include parameter names separated by commas:
+(parameter1, parameter2, ...)
+
+--The code to be executed, by the function, is placed inside curly brackets: {}
+ */
+
+function name(parameter1, parameter2, parameter3) {
+ // code to be executed
+}
+
+
+/**
+ * Function parameters are listed inside the parentheses () in the function definition.
+ * 
+ * Function arguments are the values received by the function when it is invoked.
+ * 
+ * Inside the function, the arguments (the parameters) behave as local variables.
+ */
