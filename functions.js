09d6114d0ed99f8658023f48d0f1e2d155c6aa11
atmabdullaham <atmabdullaham@gmail.com>
2024-11-26 23:45:11 +0600

    add a function called myFunction

--- functions.js
+++ functions.js
@@ -3,3 +3,10 @@
  * 
  * A JavaScript functions is executed when "something " invokes it.
  */
+// Function to compute the product of p1 and p2
+
+function myFunction(p1, p2) {
+ return p1 * p2;
+}
+
+console.log(myFunction(10, 2));
